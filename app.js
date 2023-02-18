ee6777a63b9c910e8f5d44b88c5d0a68eff30b7b
avengingtooth <esellin@gmail.com>
2023-02-18 16:44:51 +0100

    completedWithoutStyling

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
 
 const app = express();
 
@@ -12,7 +13,51 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/artist-search', (req, res, next) => {
+    console.log(req.query.artist)
+    const artist = req.query.artist
+    spotifyApi
+    .searchArtists(artist)
+    .then(data => {
+        // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        res.render('artist-search-results', {artist: data.body.artists.items})
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/albums/:artistId', (req, res, next) => {
+    spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then(data => {
+        console.log(data.body.items[0])
+        res.render('albums', {albums: data.body.items})
+    })
+    .catch(err => console.log(err))
+})
+
+app.get('/tracks/:albumId', (req, res, next) => {
+    spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then(data => {
+        console.log(data.body.items)
+        res.render('tracks', {tracks: data.body.items})
+    }) 
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
